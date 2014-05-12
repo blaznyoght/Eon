8ba05c34e69e91c1769747c84f36abb7b2b3b6af
Blaznyoght <blaznyoght@gmail.com>
2014-05-12 17:54:29 +0200

    Nouveaux projets

---
+++ CapacitanceMeter/capacitancemeter.cmd
@@ -0,0 +1,31 @@
+# Pin name action command file
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element A3
+
+# Start of element A2
+
+# Start of element A1
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element V1
+ChangePinName(V1, 2, -)
+ChangePinName(V1, 1, +)
